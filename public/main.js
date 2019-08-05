e1d3110e61dd4bed4924f49db7588fad3b8660ca
Adam Jones <adam@linuxtampa.com>
2019-08-05 16:05:22 -0400

    All button are functioning!

--- public/main.js
+++ public/main.js
@@ -5,3 +5,65 @@ const main = () => {
 }
 
 document.addEventListener('DOMContentLoaded', main)
+
+const updateName = () => {
+  //Get current name of team from a variable
+  const teamName = document.querySelector('.team-1-input').value
+  //Display new name
+  document.querySelector('.team-1-name').textContent = teamName
+}
+document
+  .querySelector('.update-team-1-name')
+  .addEventListener('click', updateName)
+
+const addTeamOneScore = () => {
+  //Save score to a variable
+  let score = document.querySelector('.team-1-score').textContent
+  // parseInt(score).value
+  console.log(score)
+  //Add 1 to score variable
+  score = parseInt(score) + 1
+  //Display updated score
+  document.querySelector('.team-1-score').textContent = score
+}
+document
+  .querySelector('.team-1-add-1-button')
+  .addEventListener('click', addTeamOneScore)
+
+const subTeamOneScore = () => {
+  //Get value of team one score
+  let score = document.querySelector('.team-1-score').textContent
+  //Subtract 1 from score
+  score = parseInt(score) - 1
+  //Display updated score
+  document.querySelector('.team-1-score').textContent = score
+}
+document
+  .querySelector('.team-1-subtract-1-button')
+  .addEventListener('click', subTeamOneScore)
+
+const updateName2 = () => {
+  const teamName2 = document.querySelector('.team-2-input').value
+  document.querySelector('.team-2-name').textContent = teamName2
+}
+document
+  .querySelector('.update-team-2-name')
+  .addEventListener('click', updateName2)
+
+const addTeamTwoScore = () => {
+  score = document.querySelector('.team-2-score').textContent
+  score = parseInt(score) + 1
+  document.querySelector('.team-2-score').textContent = score
+}
+document
+  .querySelector('.team-2-add-1-button')
+  .addEventListener('click', addTeamTwoScore)
+
+const subTeamTwoScore = () => {
+  score = document.querySelector('.team-2-score').textContent
+  score = parseInt(score) - 1
+  document.querySelector('.team-2-score').textContent = score
+}
+document
+  .querySelector('.team-2-subtract-1-button')
+  .addEventListener('click', subTeamTwoScore)
